d2e68b8241c26e0e87584137e966b5e976c1962f
Sergio CONGIA <sergio.congia@sap.com>
2022-08-15 06:34:41 +0000

    Update data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (longitude, latitude, callback) =>{
             callback('Unable to find location',undefined)
         } else {
             data = body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + 
-                     ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.'
+                     ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' +  body.current.humidity
             callback(undefined,data)
         }
     })
